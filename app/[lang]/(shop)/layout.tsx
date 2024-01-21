aa306c307694cb422d7d85a654533fc8317f0f43
SonDC <dangchison@gmail.com>
2024-01-21 20:58:16 +0700

    add AppBarSearch

--- app/[lang]/(shop)/layout.tsx
+++ app/[lang]/(shop)/layout.tsx
@@ -1,8 +1,11 @@
 import type { AppRootState } from '@/interfaces/common.interface'
 import { Fragment } from 'react'
+import dynamic from 'next/dynamic'
+
+const AppBarSearch = dynamic(() => import('@/components/AppBarSearch'), { ssr: false })
+
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 
@@ -14,6 +17,7 @@ const ShopLayout = ({ children }: AppRootState) => {
           <IconButton size='small' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
+          <AppBarSearch />
         </Toolbar>
       </AppBar>
       <main>{children}</main>
---
+++ components/AppBarSearch.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { styled, alpha } from '@mui/material/styles'
+import InputBase from '@mui/material/InputBase'
+import SearchIcon from '@mui/icons-material/Search'
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
+  },
+  marginRight: 0,
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    // marginLeft: theme.spacing(3),
+    // width: 'auto'
+  }
+}))
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    fontSize: '14px',
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('lg')]: {
+      width: '20ch'
+    }
+  }
+}))
+
+const AppBarSearch = () => {
+  return (
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase placeholder='Search…' inputProps={{ 'aria-label': 'search' }} />
+    </Search>
+  )
+}
+
+export default AppBarSearch
